a2532c584e58bb9c7fe00ec6567fb082b16b859e
jdbellamy <jdbellamy@me.com>
2012-07-26 03:51:48 -0400

    added JSR-303 validation (experimental 0.3)

---
+++ src/main/java/com/subscribers/domain/exceptions/PhoneNumberValidationException.java
@@ -0,0 +1,8 @@
+package com.subscribers.domain.exceptions;
+
+public class PhoneNumberValidationException extends Exception {
+
+	public PhoneNumberValidationException() {
+		super("100: Invalid subscriber definition!");
+	}
+}
